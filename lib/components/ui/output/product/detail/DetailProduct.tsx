c22c3c8cd60baae8fd8a91d9cda2a685a04717f5
Grigoriev Aleksander <akr4log@gmail.com>
2023-04-12 19:47:14 +0300

    Fix bug #6

--- lib/components/ui/output/product/detail/DetailProduct.tsx
+++ lib/components/ui/output/product/detail/DetailProduct.tsx
@@ -1,41 +1,42 @@
-'use client';
+// 'use client';
 
-import { useState } from 'react';
+// import { IProduct } from '@/lib/types/product.interface';
+// import { useState } from 'react';
 
-const ImagesDetailProduct = ({ images }: { images: string[] }) => {
-	const [currentIndex, setCurrentIndex] = useState(0) || [0];
-	const goToSlide = (slideIndex: number) => {
-		setCurrentIndex(slideIndex);
-	};
-	return (
-		<div className='shrink-0'>
-			<div className='h-28 w-28 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
-				<img
-					src={images[currentIndex]}
-					alt={images[currentIndex]}
-					className='h-full w-full object-cover object-center'
-				/>
-			</div>
-			{images?.length === 1 ? (
-				<div></div>
-			) : (
-				<div className='grid grid-cols-4 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-3 py-3'>
-					{images?.map((slide, slideIndex) => (
-						<div
-							key={slideIndex}
-							onClick={() => goToSlide(slideIndex)}
-							className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'
-						>
-							<img
-								className='h-full w-full mx-3 object-cover object-center'
-								src={slide}
-								alt={slide}
-							/>
-						</div>
-					))}
-				</div>
-			)}
-		</div>
-	);
-};
-export default ImagesDetailProduct;
+// const ImagesDetailProduct = ({images}:) => {
+// 	const [currentIndex, setCurrentIndex] = useState(0);
+// 	const goToSlide = (slideIndex: number) => {
+// 		setCurrentIndex(slideIndex);
+// 	};
+// 	return (
+// 		<div className='shrink-0'>
+// 			<div className='h-28 w-28 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+// 				<img
+// 					src={images[currentIndex]}
+// 					alt={images[currentIndex]}
+// 					className='h-full w-full object-cover object-center'
+// 				/>
+// 			</div>
+// 			{images?.length === 1 ? (
+// 				<div></div>
+// 			) : (
+// 				<div className='grid grid-cols-4 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-3 py-3'>
+// 					{images?.map((slide, slideIndex) => (
+// 						<div
+// 							key={slideIndex}
+// 							onClick={() => goToSlide(slideIndex)}
+// 							className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'
+// 						>
+// 							<img
+// 								className='h-full w-full mx-3 object-cover object-center'
+// 								src={slide}
+// 								alt={slide}
+// 							/>
+// 						</div>
+// 					))}
+// 				</div>
+// 			)}
+// 		</div>
+// 	);
+// };
+// export default ImagesDetailProduct;
--- pages/product/detail/DetailProduct.tsx
+++ pages/product/detail/DetailProduct.tsx
@@ -1,11 +1,44 @@
-import ImagesDetailProduct from '@/lib/components/ui/output/product/detail/DetailProduct';
+'use client'
+
 import { IProduct } from '@/lib/types/product.interface';
 import { formatToCurrency } from '@/lib/utils/format-to-currency';
+import { useState } from 'react';
 
 const DetailProduct = ({ name, description, images, price }: IProduct) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const goToSlide = (slideIndex: number) => {
+		setCurrentIndex(slideIndex);
+	};
 	return (
 		<div className='flex relative'>
-			<ImagesDetailProduct images={images} />
+			<div className='shrink-0'>
+				<div className='h-28 w-28 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+					<img
+						src={images[currentIndex]}
+						alt={images[currentIndex]}
+						className='h-full w-full object-cover object-center'
+					/>
+				</div>
+				{images?.length === 1 ? (
+					<div></div>
+				) : (
+					<div className='grid grid-cols-4 gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-3 py-3'>
+						{images?.map((slide, slideIndex) => (
+							<div
+								key={slideIndex}
+								onClick={() => goToSlide(slideIndex)}
+								className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'
+							>
+								<img
+									className='h-full w-full mx-3 object-cover object-center'
+									src={slide}
+									alt={slide}
+								/>
+							</div>
+						))}
+					</div>
+				)}
+			</div>
 			<div className='w-full'>
 				<div className='uppercase ml-4 tracking-wide text-xl text-indigo-500 font-semibold'>
 					{name}
